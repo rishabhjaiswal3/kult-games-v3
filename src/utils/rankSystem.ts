5baf8ba8d1c9141bf3a229d8f6e1d3bdb9f8d89f
BigClitPhobia <sidhanth6am@gmail.com>
2026-05-27 02:35:56 +0530

    chore: swap rank badge and arena token images to cropped versions

--- src/utils/rankSystem.ts
+++ src/utils/rankSystem.ts
@@ -23,7 +23,7 @@ export const RANKS: RankInfo[] = [
     shortName: "INITIATE",
     minElo: 0,
     maxElo: 1499,
-    image: "/ranks/initiate_1.png",
+    image: "/ranks/initiate_1_cropped.png",
     color: "#22c55e",
   },
   {
@@ -32,7 +32,7 @@ export const RANKS: RankInfo[] = [
     shortName: "CORPORAL",
     minElo: 1500,
     maxElo: 2999,
-    image: "/ranks/corporal_2.png",
+    image: "/ranks/corporal_2_cropped.png",
     color: "#f97316",
   },
   {
@@ -41,7 +41,7 @@ export const RANKS: RankInfo[] = [
     shortName: "CYBER LT.",
     minElo: 3000,
     maxElo: 4999,
-    image: "/ranks/cyber_lieutenant_3.png",
+    image: "/ranks/cyber_lieutenant_3_cropped.png",
     color: "#06b6d4",
   },
   {
@@ -50,7 +50,7 @@ export const RANKS: RankInfo[] = [
     shortName: "QUANTUM MAJ.",
     minElo: 5000,
     maxElo: 7499,
-    image: "/ranks/quantum_major_4.png",
+    image: "/ranks/quantum_major_4_cropped.png",
     color: "#eab308",
   },
   {
@@ -59,7 +59,7 @@ export const RANKS: RankInfo[] = [
     shortName: "NEURAL CAPT.",
     minElo: 7500,
     maxElo: 9999,
-    image: "/ranks/neural_captain_5.png",
+    image: "/ranks/neural_captain_5_cropped.png",
     color: "#a855f7",
   },
   {
@@ -68,7 +68,7 @@ export const RANKS: RankInfo[] = [
     shortName: "PROTO CMD.",
     minElo: 10000,
     maxElo: 14999,
-    image: "/ranks/protocol_commander_6.png",
+    image: "/ranks/protocol_commander_6_cropped.png",
     color: "#8b5cf6",
   },
   {
@@ -77,7 +77,7 @@ export const RANKS: RankInfo[] = [
     shortName: "GENESIS OVL.",
     minElo: 15000,
     maxElo: 24999,
-    image: "/ranks/genesis_overlord_7.png",
+    image: "/ranks/genesis_overlord_7_cropped.png",
     color: "#f59e0b",
   },
   {
@@ -86,7 +86,7 @@ export const RANKS: RankInfo[] = [
     shortName: "SINGULARITY",
     minElo: 25000,
     maxElo: null,
-    image: "/ranks/singularity_prime_8.png",
+    image: "/ranks/singularity_prime_8_cropped.png",
     color: "#818cf8",
   },
 ];
