421851950894f7463e0c483b82c097eedcc9b45e
Sid <sidhanth6am@gmail.com>
2026-07-27 21:40:47 +0530

    Update build URL from Warzone2 to Warzone3

--- src/pages/WarzoneWaveGamePage.tsx
+++ src/pages/WarzoneWaveGamePage.tsx
@@ -1396,7 +1396,7 @@ export default function WarzoneWaveGamePage() {
     };
     localStorage.setItem('arenaBattlePayload', JSON.stringify(arenaPayload));
 
-    const buildUrl = `${UNITY_BASE_URL}/Warzone2`;
+    const buildUrl = `${UNITY_BASE_URL}/Warzone3`;
     const script = document.createElement("script");
     script.src = `${buildUrl}/WarzoneV4.loader.js`;
 
